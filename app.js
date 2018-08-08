9987ddec1f963c5b1f437c376934ec21f4319acd
benalfa <mituro3268@gmail.com>
2018-08-08 22:19:43 +0900

    3rd Commit

--- app.js
+++ app.js
@@ -1,28 +1,47 @@
+'use strict';
 var http = require('http');
 var express = require('express');
 var path = require('path');
+var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
+
+var Message = require('./schema/Message');
 
 var app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-mongoose.connect('mongodb://localhose:27017/chatapp', 
+mongoose.connect('mongodb://localhost:27017/chatapp', 
 function(err){
     if(err){
         console.error(err);
-        return;
+    }else{
+        console.log('sucessfully connected to MongoDB.');
     }
-    console.log('sucessfully connected to MongoDB.');
 });
+
+app.use(bodyparser());
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.get('/', function(reqeust, response, next){
     return response.render('index',{title:'Hello World'});
 });
 
-app.get('/hoge', function(reqeust, response, next){
-    return response.send('Hoge');
+app.get('/update', function(request, response, next){
+    return response.render('update');
+});
+
+app.post('/update', function(request, response, next){
+
+    var newMessage = new Message({
+        username: request.body.username,
+        message: request.body.message
+    });
+    newMessage.save((err)=>{
+        if(err)
+            throw err;
+            return response.redirect('/');
+    });
 });
 
 var server = http.createServer(app);
