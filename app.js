2084a4927e645310e12a50a9d67e5355dd710663
benalfa <mituro3268@gmail.com>
2018-08-08 21:19:21 +0900

    2nd Commit

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var http = require('http');
 var express = require('express');
 var path = require('path');
+var mongoose = require('mongoose');
 
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+mongoose.connect('mongodb://localhose:27017/chatapp', 
+function(err){
+    if(err){
+        console.error(err);
+        return;
+    }
+    console.log('sucessfully connected to MongoDB.');
+});
 
 app.get('/', function(reqeust, response, next){
     return response.render('index',{title:'Hello World'});
